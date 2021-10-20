b57c8817d6526f1c0acf65fc4ab28ca5734c985a
emirunsal <92887790+emirunsal@users.noreply.github.com>
2021-10-21 01:36:33 +0300

    Add files via upload

---
+++ oddEvenSort/cmake-build-debug/CMakeFiles/oddEvenSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oddEvenSort.dir/main.cpp.obj"
+  "liboddEvenSort.dll.a"
+  "oddEvenSort.exe"
+  "oddEvenSort.exe.manifest"
+  "oddEvenSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oddEvenSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
